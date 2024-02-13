b933387dad0e9d6676482163c0c7232172e6ee7c
unknown <computersciencejinja@gmail.com>
2024-02-13 11:33:07 +0000

    Fourth commit

--- main/views.py
+++ main/views.py
@@ -136,47 +136,47 @@ def diagnosis(request):
 
 @csrf_exempt
 def MakePredict(request):
-	s1 = request.POST.get('s1')
-	s2 = request.POST.get('s2')
-	s3 = request.POST.get('s3')
-	s4 = request.POST.get('s4')
-	s5 = request.POST.get('s5')
-	id = request.POST.get('id')
-	
-	list_b = [s1,s2,s3,s4,s5]
-	print(list_b)
+    s1 = request.POST.get('s1')
+    s2 = request.POST.get('s2')
+    s3 = request.POST.get('s3')
+    s4 = request.POST.get('s4')
+    s5 = request.POST.get('s5')
+    patient_id = request.POST.get('id')
+    
+    list_b = [s1, s2, s3, s4, s5]
 
-	list_a = ['itching','skin_rash','nodal_skin_eruptions','continuous_sneezing','shivering','chills','joint_pain','stomach_pain','acidity','ulcers_on_tongue','muscle_wasting','vomiting','burning_micturition','spotting_ urination','fatigue','weight_gain','anxiety','cold_hands_and_feets','mood_swings','weight_loss','restlessness','lethargy','patches_in_throat','irregular_sugar_level','cough','high_fever','sunken_eyes','breathlessness','sweating','dehydration','indigestion','headache','yellowish_skin','dark_urine','nausea','loss_of_appetite','pain_behind_the_eyes','back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine','yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach','swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation','redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs','fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool','irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs','swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails','swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips','slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints','movement_stiffness','spinning_movements','loss_of_balance','unsteadiness','weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine','continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)','depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain','abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum','rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion','receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen','history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf','palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling','silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose','yellow_crust_ooze']
+    list_a = ['itching', 'skin_rash', 'nodal_skin_eruptions', 'continuous_sneezing', 'shivering', 'chills', 'joint_pain', 'stomach_pain', 'acidity', 'ulcers_on_tongue', 'muscle_wasting', 'vomiting', 'burning_micturition', 'spotting_ urination', 'fatigue', 'weight_gain', 'anxiety', 'cold_hands_and_feets', 'mood_swings', 'weight_loss', 'restlessness', 'lethargy', 'patches_in_throat', 'irregular_sugar_level', 'cough', 'high_fever', 'sunken_eyes', 'breathlessness', 'sweating', 'dehydration', 'indigestion', 'headache', 'yellowish_skin', 'dark_urine', 'nausea', 'loss_of_appetite', 'pain_behind_the_eyes', 'back_pain', 'constipation', 'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellow_urine', 'yellowing_of_eyes', 'acute_liver_failure', 'fluid_overload', 'swelling_of_stomach', 'swelled_lymph_nodes', 'malaise', 'blurred_and_distorted_vision', 'phlegm', 'throat_irritation', 'redness_of_eyes', 'sinus_pressure', 'runny_nose', 'congestion', 'chest_pain', 'weakness_in_limbs', 'fast_heart_rate', 'pain_during_bowel_movements', 'pain_in_anal_region', 'bloody_stool', 'irritation_in_anus', 'neck_pain', 'dizziness', 'cramps', 'bruising', 'obesity', 'swollen_legs', 'swollen_blood_vessels', 'puffy_face_and_eyes', 'enlarged_thyroid', 'brittle_nails', 'swollen_extremeties', 'excessive_hunger', 'extra_marital_contacts', 'drying_and_tingling_lips', 'slurred_speech', 'knee_pain', 'hip_joint_pain', 'muscle_weakness', 'stiff_neck', 'swelling_joints', 'movement_stiffness', 'spinning_movements', 'loss_of_balance', 'unsteadiness', 'weakness_of_one_body_side', 'loss_of_smell', 'bladder_discomfort', 'foul_smell_of urine', 'continuous_feel_of_urine', 'passage_of_gases', 'internal_itching', 'toxic_look_(typhos)', 'depression', 'irritability', 'muscle_pain', 'altered_sensorium', 'red_spots_over_body', 'belly_pain', 'abnormal_menstruation', 'dischromic _patches', 'watering_from_eyes', 'increased_appetite', 'polyuria', 'family_history', 'mucoid_sputum', 'rusty_sputum', 'lack_of_concentration', 'visual_disturbances', 'receiving_blood_transfusion', 'receiving_unsterile_injections', 'coma', 'stomach_bleeding', 'distention_of_abdomen', 'history_of_alcohol_consumption', 'fluid_overload', 'blood_in_sputum', 'prominent_veins_on_calf', 'palpitations', 'painful_walking', 'pus_filled_pimples', 'blackheads', 'scurring', 'skin_peeling', 'silver_like_dusting', 'small_dents_in_nails', 'inflammatory_nails', 'blister', 'red_sore_around_nose', 'yellow_crust_ooze']
 
-	# Loop to convert all symptoms into 0's
-	list_c = [] # EMpty list to store disease symptoms converted into 0's and 1's
-	for x in range(0,len(list_a)):
-		list_c.append(0)
+    # Create a dictionary to map symptoms to their corresponding index
+    symptom_to_index = {symptom: index for index, symptom in enumerate(list_a)}
 
-	print(list_c)
+    # Initialize a list to store disease symptoms as binary values
+    disease_symptoms = [0] * len(list_a)
 
+    # Convert input symptoms into binary values
+    for symptom in list_b:
+        index = symptom_to_index.get(symptom)
+        if index is not None:
+            disease_symptoms[index] = 1
 
-	# For all matched specific disease symptoms in general put 1 and unmatched put 0
-	for z in range(0,len(list_a)):
-		for k in list_b:
-			if(k==list_a[z]):
-				list_c[z]=1
+    # Convert the list of symptoms into a numpy array
+    test_data = np.array(disease_symptoms).reshape(1, -1)
 
-	test = list_c
-	test = np.array(test)
-	test = np.array(test).reshape(1,-1)
-	print(test.shape)
+    # Load the trained decision tree model
+    clf = joblib.load('model/final_decision_tree_model.pkl')
 
-	clf = joblib.load('model/final_decision_tree_model.pkl')
+    # Make prediction using the model
+    prediction = clf.predict(test_data)
 
-	prediction = clf.predict(test)
-	result = prediction[0]
+    # Retrieve the predicted disease name based on the index
+    predicted_disease = list_a[prediction[0]]
 
-	a = Medical(s1=s1, s2=s2, s3=s3, s4=s4, s5=s5, disease=result, patient_id=id)
-	a.save()
+    # Save the prediction to the database
+    medical_record = Medical(s1=s1, s2=s2, s3=s3, s4=s4, s5=s5, disease=predicted_disease, patient_id=patient_id)
+    medical_record.save()
 
-	return JsonResponse({'status':result})			
-
+    # Return the prediction as a JSON response
+    return JsonResponse({'status': predicted_disease})
 
 
 def patient_result(request):
